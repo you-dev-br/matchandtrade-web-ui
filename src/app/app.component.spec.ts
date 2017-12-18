3a2fbd4ff02c4437bed66d4c3b994d2326d8ad16
Rafael Santos <rafael.santos.bra@gmail.com>
2017-12-17 22:04:13 -0500

    Updated app.component.spec.ts tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -8,24 +10,17 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavigationBarComponent
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should contain <app-navigation-bar>', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    const appElement: HTMLElement = fixture.nativeElement;
+    const appNavigationBar: HTMLElement = appElement.querySelector("app-navigation-bar");
+    expect(appNavigationBar).toBeDefined();
   }));
 });
