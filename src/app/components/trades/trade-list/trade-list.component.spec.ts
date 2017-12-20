1982ae32964549bf81bde40501c19511224adf62
Rafael Santos <rafael.santos.bra@gmail.com>
2017-12-19 19:55:44 -0500

    Updated tests on trade-list.component.spec.ts

--- src/app/components/trades/trade-list/trade-list.component.spec.ts
+++ src/app/components/trades/trade-list/trade-list.component.spec.ts
@@ -1,12 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Pagination } from '../../../classes/search/pagination';
 import { PaginationComponent } from '../../pagination/pagination.component';
-import { TradeListComponent } from './trade-list.component';
 import { RouterOutletStubComponent, RouterLinkStubDirective } from '../../../../test/router-stubs';
+import { TradeListComponent } from './trade-list.component';
+import { Trade } from '../../../classes/pojo/trade';
 
 describe('TradeListComponent', () => {
   let component: TradeListComponent;
   let fixture: ComponentFixture<TradeListComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -24,11 +27,24 @@ describe('TradeListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('when there are trades; then should display rows', (async(() => {
+    let t1 = new Trade();
+    t1.tradeId = 1;
+    t1.name = "Trade number 1";
 
-  it('should contain row', () => {
-    expect(fixture.nativeElement.querySelector('td')).toBeTruthy();
-  });
+    component.trades = [t1];
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(fixture.nativeElement.querySelector('td')).toBeTruthy();
+    });
+  })));
+
+  it('when there are no trades; then should not display any rows', (async(() => {
+    component.trades = [];
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(fixture.nativeElement.querySelector('td')).toBeNull();
+    });
+  })));
+
 });
