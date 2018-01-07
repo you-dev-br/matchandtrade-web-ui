f00b0219779b1021ac4a9c7eafa215a2f3c166ee
Rafael Santos <rafael.santos.bra@gmail.com>
2018-01-06 21:59:53 -0500

    Added trade.component.spec.ts

--- src/app/components/trades/trade/trade.component.spec.ts
+++ src/app/components/trades/trade/trade.component.spec.ts
@@ -1,41 +1,56 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthenticationService } from '../../../services/authentication.service';
 import { Pagination } from '../../../classes/search/pagination';
 import { PaginationComponent } from '../../pagination/pagination.component';
-import { RouterOutletStubComponent, RouterLinkStubDirective, RouterStub } from '../../../../test/router-stubs';
-import { TradeListComponent } from './trade-list.component';
+import { RouterOutletStubComponent, RouterLinkStubDirective, RouterStub, ActivatedRouteStub, ActivatedRoute } from '../../../../test/router-stubs';
+import { TradeComponent } from './trade.component';
 import { Trade } from '../../../classes/pojo/trade';
 import { MessageComponent } from '../../message/message.component';
 import { LoadingComponent } from '../../loading/loading.component';
 import { TradeService } from '../../../services/trade.service';
-import { RouterStateSnapshot } from '@angular/router/src/router_state';
+import { RouterStateSnapshot } from '@angular/router';
 import { SearchResult } from '../../../classes/search/search-result';
 
 class TradeServiceMock {
   search(){
     return new Promise<SearchResult<Trade>>(() => {});
   };
+  get() {
+    return new Promise<SearchResult<Trade>>(() => {});
+  }
 }
 
-describe('TradeListComponent', () => {
-  let component: TradeListComponent;
-  let fixture: ComponentFixture<TradeListComponent>;
+const activatedRouteMock = {
+    snapshot: {
+        params: {tradeId: 'CREATE'},
+        paramMap: {
+            get: function(){ return 'hrefMock'}
+        }
+    }
+}
+
+describe('TradeComponent', () => {
+  let component: TradeComponent;
+  let fixture: ComponentFixture<TradeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [
         MessageComponent,
         LoadingComponent,
-        TradeListComponent,
+        TradeComponent,
         PaginationComponent,
         RouterLinkStubDirective,
         RouterOutletStubComponent ],
-        }).overrideComponent(TradeListComponent, {
+        }).overrideComponent(TradeComponent, {
           set: {
             providers:[
               {provide: Router, useClass: RouterStub },
+              {provide: ActivatedRoute, useValue: activatedRouteMock},
               {provide: TradeService, useClass: TradeServiceMock}]
           }
         })
@@ -43,28 +58,17 @@ describe('TradeListComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TradeListComponent);
+    fixture = TestBed.createComponent(TradeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('when there are trades; then should display rows', (async(() => {
-    let t1 = new Trade();
-    t1.tradeId = 1;
-    t1.name = "Trade number 1";
-
-    component.trades = [t1];
+  it('when creating a new trade; then it should display an empty form', (async(() => {
     fixture.detectChanges();
     fixture.whenStable().then(() => {
-      expect(fixture.nativeElement.querySelector('td')).toBeNull();
-    });
-  })));
-
-  it('when there are no trades; then should not display any rows', (async(() => {
-    component.trades = [];
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      expect(fixture.nativeElement.querySelector('td')).toBeNull();
+        expect(fixture.nativeElement.querySelector('#trade-name').value).toBeFalsy();        
+        expect(fixture.nativeElement.querySelector('#trade-state').value).toBeFalsy();
+        expect(fixture.nativeElement.querySelector('#trade-state').disabled).toBeTruthy();        
     });
   })));
 
