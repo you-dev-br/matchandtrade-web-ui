bb8977c5b6df9aa0e5409614a71457e6d08d149e
Rafael Santos <rafael.santos.bra@gmail.com>
2018-02-03 20:47:41 -0500

    Added matching items component - Round 1 Added matching items component - Round 2 - SearchCriteria Added matching items component - Round 3 - OfferingItems Added matching items component - Round 4 - Item Matcher rendering Added matching items component - Round 4 - Refactored Item Matcher into Item Matcher List Added matching items component - Round 6 - Removed Offer Item Component Added basic navigation to item-matcher-offer.component

---
+++ src/app/components/item-matcher-list/item-matcher-list.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { Message } from '../message/message';
+import { Item } from '../../classes/pojo/item';
+import { SearchService } from '../../services/search.service';
+import { TradeMembership } from '../../classes/pojo/trade-membership';
+import { TradeMembershipService } from '../../services/trade-membership.service';
+import { Pagination } from '../../classes/search/pagination';
+
+@Component({
+  selector: 'app-item-matcher-list',
+  templateUrl: './item-matcher-list.component.html',
+  styleUrls: ['./item-matcher-list.component.scss'],
+  providers: [TradeMembershipService, SearchService]
+})
+export class ItemMatcherListComponent implements OnInit {
+
+	items: Item[] = Array<Item>();
+  loading: boolean = true;
+  message: Message = new Message();
+  pagination: Pagination = new Pagination(1, 5, 0);
+  tradeMembership: TradeMembership;
+  tradeMembershipHref: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private searchService: SearchService,
+    private tradeMembershipService: TradeMembershipService,
+  ) { }
+
+  ngOnInit() {
+    this.tradeMembershipHref = this.route.snapshot.paramMap.get('tradeMembershipHref');
+
+    this.tradeMembershipService.get(this.tradeMembershipHref)
+      .then(tradeMembership => {
+        this.tradeMembership = tradeMembership;
+        return tradeMembership;
+      })
+      .then(tradeMembership => {
+        this.search(tradeMembership);
+			})
+      .catch(e => {
+        this.message.setErrorItems(e);
+      })
+      .then(() => {
+        this.loading = false;
+      });
+  }
+  
+  nextPage() {
+      this.pagination.page.number++;
+      this.loading = true;
+      this.search(this.tradeMembership).then(() => this.loading = false);
+  }
+
+  previousPage() {
+      this.pagination.page.number--;
+      this.loading = true;
+      this.search(this.tradeMembership).then(() => this.loading = false);
+  }
+
+  search(tradeMembership: TradeMembership) {
+    return this.searchService.searchItemsToMatch(tradeMembership, this.pagination.page).then(searchResults => {
+      this.items = searchResults.results;
+      this.pagination = searchResults.pagination;
+    });
+  }
+
+  navigateToOffer(item: Item) {
+    this.router.navigate(['item-matcher-offer', {itemHref: item._href}]);
+  }
+
+}
