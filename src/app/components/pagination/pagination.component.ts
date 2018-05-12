4ad048f34ef4947d954b0dc06fa72c86e73d964e
Rafael Santos <rafael.santos.bra@gmail.com>
2018-05-12 19:45:58 -0400

    Redesigned pagination.component

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 import { Pagination } from '../../classes/search/pagination';
+import { KeyValuePair } from '../../classes/pojo/key-value-pair';
 
 export enum PaginationButton{NEXT, NONE, PREVIOUS}
 
@@ -9,28 +10,86 @@ export enum PaginationButton{NEXT, NONE, PREVIOUS}
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit {
   @Input() loading: boolean = false;
   @Input() pagination: Pagination;
-  @Output() onNextPage: EventEmitter<Pagination>;
-  @Output() onPreviousPage: EventEmitter<Pagination>;
-  private lastButton: PaginationButton = PaginationButton.NONE;
+  @Output() onNextPage = new EventEmitter<Pagination>();
+  @Output() onPreviousPage = new EventEmitter<Pagination>();
+	interval: any[] = [];
+	currentPage: number;
 
-  constructor() {
-    this.onNextPage = new EventEmitter<Pagination>();
-    this.onPreviousPage = new EventEmitter<Pagination>();
-  }
+  constructor() {}
+	
+	ngOnInit() {
+		const totalPages = this.pagination.totalPages;
+		// Only display the pagination if there is more than one page
+		if (totalPages < 2) {
+			return;
+		}
+		
+		this.currentPage = this.pagination.page.number;
+		const pageNumber = this.pagination.page.number;
+		const maxPagesToDisplay = 5;
+
+		// First, we insert the current page
+		this.addToPageInterval(pageNumber, maxPagesToDisplay, totalPages);
+
+		// When page number is the first page, we populate pageInterval in ascending order
+		if(pageNumber == 1) {
+			for(let i=2; i<=maxPagesToDisplay; i++) {
+				this.addToPageInterval(i, maxPagesToDisplay, totalPages);
+			}
+		}
+		// When page number is the last page, we populate pageInterval in descending order
+		else if(pageNumber == totalPages) {
+			for(let i=totalPages-1; i>totalPages-maxPagesToDisplay; i--) {
+				this.addToPageInterval(i, maxPagesToDisplay, totalPages);
+			}
+		}
+		// When page number is somewhere in the middle, we populate rigth and left
+		else {
+			for(let i=1; i<maxPagesToDisplay; i++) {
+				this.addToPageInterval(pageNumber+i, maxPagesToDisplay, totalPages);				
+				this.addToPageInterval(pageNumber-i, maxPagesToDisplay, totalPages);
+			}
+		}
+
+		// Sort it numerically. See: https://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly
+		this.interval.sort((a, b) => a - b);
+
+		// Add heading ellipis
+		if (totalPages > maxPagesToDisplay && this.currentPage > Math.ceil(maxPagesToDisplay/2)) {
+			this.interval.unshift('...');
+		}
+
+		// Add tailing ellipsis
+		if (this.interval[this.interval.length-1] < totalPages) {
+			this.interval.push('...');
+		}
+
+	}
+
+	private addToPageInterval(pageNumber: number, maxIntervalSize: number, totalPages) {
+		if (this.interval.length < maxIntervalSize && pageNumber > 0 && pageNumber <= totalPages) {
+			this.interval.push(pageNumber);
+		}
+	}
 
   nextPage() {
     if (this.isNextPageButtonEnabled()) {
-      this.lastButton = PaginationButton.NEXT;
       this.onNextPage.emit(this.pagination);
     }
-  }
+	}
+	
+	pageClass(page: any): string {
+		let result = 'button is-small page ';
+		result += (page==this.currentPage ? 'current-page' : ' ');
+		result += (page=='...' ? 'ellipsis' : '');
+		return result;
+	}
 
-  previousPage() {
+	previousPage() {
     if (this.isPreviousPageButtonEnabled()) {
-      this.lastButton = PaginationButton.PREVIOUS;
       this.onPreviousPage.emit(this.pagination);
     }
   }
@@ -38,7 +97,8 @@ export class PaginationComponent {
   isNextPageButtonEnabled(): boolean {
     if (!this.loading
         && this.pagination
-        && this.pagination.page.number * this.pagination.page.size < this.pagination.totalEntries ) {
+				&& this.pagination.page
+				&& this.pagination.page.number * this.pagination.page.size < this.pagination.totalEntries ) {
       return true;
     }
     return false;
@@ -47,26 +107,11 @@ export class PaginationComponent {
   isPreviousPageButtonEnabled(): boolean {
     if (!this.loading
       && this.pagination
-      && this.pagination.page.number > 1) {
+      && this.pagination.page
+			&& this.pagination.page.number > 1) {
       return true;
     }
     return false;
   }
- 
-  isNextPageLoading(): boolean {
-    if(this.loading && this.lastButton == PaginationButton.NEXT) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  isPreviousPageLoading(): boolean {
-    if(this.loading && this.lastButton == PaginationButton.PREVIOUS) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
+	
 }
