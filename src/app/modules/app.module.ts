bf22238388c897e61c527dd12af58af002ec1bec
Rafael Santos <rafael.santos.bra@gmail.com>
2018-12-09 17:59:42 -0500

    Rearranged modules

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './routing.module';
+import { AppComponent } from '../app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialDesignModule } from './modules/material-design.module';
+import { MaterialDesignModule } from './material-design.module';
 
 @NgModule({
   declarations: [
