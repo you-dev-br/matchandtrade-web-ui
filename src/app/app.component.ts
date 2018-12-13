97b6b66dcefe90df6e02d8d8b4d5fadcc5656fbd
Rafael Santos <rafael.santos.bra@gmail.com>
2018-12-12 23:05:32 -0500

    Added material design theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	isMobileMenuExpanded = true;
+	isMobileMenuExpanded = false;
 	
 	classMobileMenu(): string {
 		let expanded = this.isMobileMenuExpanded ? 'menu-mobile-expanded' : '';
