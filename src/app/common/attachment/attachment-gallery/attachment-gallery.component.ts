8a3f55290beba10cebf1d4bb11f8771b35fd2fc8
Rafael Santos <rafael.santos.bra@gmail.com>
2019-02-16 10:20:28 -0500

    Added loading spinner when loading images in attachment-gallery

--- src/app/common/attachment/attachment-gallery/attachment-gallery.component.ts
+++ src/app/common/attachment/attachment-gallery/attachment-gallery.component.ts
@@ -12,16 +12,28 @@ export class AttachmentGalleryComponent {
   attachments: Attachment[] = [];
   @Output()
   onDeleteError = new EventEmitter<Error>();
+  private loadedAttachments: string[] = [];
 
   constructor(private attachmentService: AttachmentService) { }
 
   async onDelete(attachment: Attachment): Promise<void> {
+    console.log('delete', this.loadedAttachments);
     try {
+      this.loadedAttachments.splice(this.loadedAttachments.indexOf(attachment.attachmentId), 1);
       await this.attachmentService.delete(attachment.getSelfHref());
       this.attachments.splice(this.attachments.indexOf(attachment), 1);
     } catch (e) {
       this.onDeleteError.emit(e);
     }
+  }
+
+  isAttachmentLoaded(attachment): boolean {
+    console.log('isLoaded', this.loadedAttachments);
+    return this.loadedAttachments.includes(attachment.attachmentId);
+  }
+
+  onLoadAttachment(attachment: Attachment): void {
+    this.loadedAttachments.push(attachment.attachmentId);
   }
 
   onUploadComplete(attachment: Attachment): void {
