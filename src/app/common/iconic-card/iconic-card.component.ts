0052e67cf2af5489392cba2406a1fca4f694570a
Rafael Santos <rafael.santos.bra@gmail.com>
2019-02-17 23:06:40 -0500

    Refactor iconic-card

--- src/app/common/iconic-card/iconic-card.component.ts
+++ src/app/common/iconic-card/iconic-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChildren, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-iconic-card',
@@ -7,8 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class IconicCardComponent implements OnInit {
   @Input()
-  visible: boolean;
-  
+  icon: string = '/assets/icons8-snail-48.png';
+  @Input()
+  title!: string;
+
   constructor() { }
 
   ngOnInit() {
