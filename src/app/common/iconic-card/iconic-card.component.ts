91757217fa4fabe4aa248ca3789f0127bad238f7
Rafael Santos <rafael.santos.bra@gmail.com>
2019-02-18 21:13:37 -0500

    Moved icons to assets/icons folder

--- src/app/common/iconic-card/iconic-card.component.ts
+++ src/app/common/iconic-card/iconic-card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, ViewChildren, ElementRef } from '@angular/cor
 })
 export class IconicCardComponent implements OnInit {
   @Input()
-  icon: string = '/assets/icons8-snail-48.png';
+  icon: string = '/assets/icons/icons8-snail-48.png';
   @Input()
   title!: string;
 
